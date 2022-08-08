c9f872888ce96145a5eaadabb8b2328be2e82502
Alena <100042777+AlenaKabushko@users.noreply.github.com>
2022-08-08 18:22:00 +0300

    01. add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+//console.log(galleryRef)
+
+//create elements===========================================================
+
+for (const galleryItem of galleryItems) {
+    const { preview, original, description } = galleryItem;
+
+    const addToGallery = [];
+
+    const galleryItemRef = document.createElement("div");
+    galleryItemRef.classList.add("gallery__item")
+
+    const galleryLinkRef = document.createElement("a");
+    galleryLinkRef.classList.add("gallery__link")
+    galleryLinkRef.setAttribute("href", `${original}`);
+
+    const galleryImageRef = document.createElement("img");
+    galleryImageRef.classList.add("gallery__image");
+    galleryImageRef.setAttribute("src", `${preview}`);
+    galleryImageRef.setAttribute("data-source", `${original}`);
+    galleryImageRef.setAttribute("alt", `${description}`);
+
+    galleryItemRef.appendChild(galleryLinkRef);
+    galleryLinkRef.appendChild(galleryImageRef);
+
+    //console.log(galleryItemRef);
+
+    addToGallery.push(galleryItemRef);
+    galleryRef.append(...addToGallery);
+}
+
+
+
+
+
+
+
+
+
