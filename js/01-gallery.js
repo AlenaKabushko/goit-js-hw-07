3ac38c737ee1e2a0921583bb732a0eb5c0964e16
Alena <100042777+AlenaKabushko@users.noreply.github.com>
2022-08-10 16:36:26 +0300

    add 01 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,42 +3,85 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const galleryRef = document.querySelector(".gallery");
-//console.log(galleryRef)
+console.log(galleryRef)
 
 //create elements===========================================================
 
-for (const galleryItem of galleryItems) {
-    const { preview, original, description } = galleryItem;
+// for (const galleryItem of galleryItems) {
+//     const { preview, original, description } = galleryItem;
 
-    const addToGallery = [];
+//     const addToGallery = [];
 
-    const galleryItemRef = document.createElement("div");
-    galleryItemRef.classList.add("gallery__item")
+//     const galleryItemRef = document.createElement("div");
+//     galleryItemRef.classList.add("gallery__item")
 
-    const galleryLinkRef = document.createElement("a");
-    galleryLinkRef.classList.add("gallery__link")
-    galleryLinkRef.setAttribute("href", `${original}`);
+//     const galleryLinkRef = document.createElement("a");
+//     galleryLinkRef.classList.add("gallery__link")
+//     galleryLinkRef.setAttribute("href", `${original}`);
 
-    const galleryImageRef = document.createElement("img");
-    galleryImageRef.classList.add("gallery__image");
-    galleryImageRef.setAttribute("src", `${preview}`);
-    galleryImageRef.setAttribute("data-source", `${original}`);
-    galleryImageRef.setAttribute("alt", `${description}`);
+//     const galleryImageRef = document.createElement("img");
+//     galleryImageRef.classList.add("gallery__image");
+//     galleryImageRef.setAttribute("src", `${preview}`);
+//     galleryImageRef.setAttribute("data-source", `${original}`);
+//     galleryImageRef.setAttribute("alt", `${description}`);
 
-    galleryItemRef.appendChild(galleryLinkRef);
-    galleryLinkRef.appendChild(galleryImageRef);
+//     galleryItemRef.appendChild(galleryLinkRef);
+//     galleryLinkRef.appendChild(galleryImageRef);
 
-    //console.log(galleryItemRef);
+//     //console.log(galleryItemRef);
 
-    addToGallery.push(galleryItemRef);
-    galleryRef.append(...addToGallery);
+//     addToGallery.push(galleryItemRef);
+//     galleryRef.append(...addToGallery);
+// }
+
+function createElementToGallery(galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>`;
+    }).join('');
+};
+
+const addToGallery = createElementToGallery(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", addToGallery);
+
+//library======================
+
+function openModal(event) {
+    event.preventDefault()
+
+    if (event.target.classList.contains("gallery")) {
+        return
+    }
+
+    // console.log('open modal');
+    // console.log(event.target);
+    // console.log(event.target.classList.contains("gallery"));
+
+    const instance = basicLightbox.create(`
+		<img src="${event.target.dataset.source}">
+	`)
+
+    instance.show()
+
+    function closeModal(e) {
+        if (e.key === "Escape") {
+            //console.log("ravno")
+            instance.close()
+        }
+    }
+    
+    document.addEventListener("keydown", closeModal)
+
 }
 
-
-
-
-
-
-
-
+galleryRef.addEventListener("click", openModal);
 
